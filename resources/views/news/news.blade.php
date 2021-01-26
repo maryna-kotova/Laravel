f2858d6a57896cb6634599556c21639154a2ecdd
Kotova Maryna <marynakotova256@gmail.com>
2021-01-26 02:26:41 +0200

    style-news

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -8,13 +8,14 @@
     </div>
 
     @foreach ($news as $new)
-        <article class="news">           
+        <div class="news">           
             <img src="{{ $new->img }}" alt="{{ $title }}">   
-            <h4>{{ $new->title }}</h4>    
-            <p>{{ $new->created_at}}</p>    
-            <p>{{ $new->short_content }}</p>
-            
-        </article>
+            <article>
+                <h3>{{ $new->title }}</h3>    
+                <p>{{ $new->created_at}}</p>    
+                <p>{{ $new->short_content }}</p>
+            </article>            
+        </div>
     @endforeach
 
     {{$news->links()}}
