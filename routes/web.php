fff2cb11be2f084c43f8f7a9c6977999916213d2
Kotova Maryna <marynakotova256@gmail.com>
2021-01-30 15:02:36 +0200

    admin-tools

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ReviewController;
@@ -23,15 +25,19 @@ use Illuminate\Support\Facades\Auth;
 //     return view('welcome');
 // });
 // Route::get('/', 'MainController@index'); - laravel before 8 version
-Route::get( '/',                [MainController::class,     'index']);
-Route::get( '/contacts',        [MainController::class,     'contacts'])->middleware('auth');
-Route::post('/contacts',        [MainController::class,     'getContacts']);
-Route::get( '/sale',            [StoreController::class,    'sale']);
-Route::get( '/reviews',         [ReviewController::class,   'reviews']);
-Route::post('/reviews',         [ReviewController::class,   'saveReview']);
-Route::get( '/news',            [NewsController::class,     'news']);
+Route::get( '/',                [MainController::class,   'index']);
+Route::get( '/contacts',        [MainController::class,   'contacts'])->middleware('auth');
+Route::post('/contacts',        [MainController::class,   'getContacts']);
+Route::get( '/sale',            [StoreController::class,  'sale']);
+Route::get( '/reviews',         [ReviewController::class, 'reviews']);
+Route::post('/reviews',         [ReviewController::class, 'saveReview']);
+Route::get( '/news',            [NewsController::class,   'news']);
+Route::get( '/category/{slug}', [StoreController::class,  'category']);
 
-Route::get( '/category/{slug}', [StoreController::class, 'category']);
+Route::middleware(['auth'])->prefix('admin')->group(function(){
+   Route::get('/',  [AdminController::class,  'index']);
+   Route::resource('/category', CategoryController::class);
+});
 
 Auth::routes();
 
